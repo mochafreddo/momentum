49f61b2db0885a332e6bf6375ec1f5765e74be0a
mochafreddo <76798197+mochafreddo@users.noreply.github.com>
2023-03-19 00:06:47 +0900

    6.0 Quotes

---
+++ js/quotes.js
@@ -0,0 +1,52 @@
+const quotes = [
+  {
+    quote: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt",
+  },
+  {
+    quote: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+  },
+  {
+    quote: "I have not failed. I've just found 10,000 ways that won't work.",
+    author: "Thomas Edison",
+  },
+  {
+    quote:
+      "In three words I can sum up everything I've learned about life: it goes on.",
+    author: "Robert Frost",
+  },
+  {
+    quote: "The best way to predict the future is to invent it.",
+    author: "Alan Kay",
+  },
+  {
+    quote: "Life is 10% what happens to us and 90% how we react to it.",
+    author: "Charles R. Swindoll",
+  },
+  {
+    quote: "If you want to lift yourself up, lift up someone else.",
+    author: "Booker T. Washington",
+  },
+  {
+    quote:
+      "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+    author: "Winston Churchill",
+  },
+  {
+    quote: "The only true wisdom is in knowing you know nothing.",
+    author: "Socrates",
+  },
+  {
+    quote: "We must be the change we wish to see in the world.",
+    author: "Mahatma Gandhi",
+  },
+];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote;
+author.innerText = todaysQuote.author;
