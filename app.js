bcfaf27d61ae0b44506ebf167fb956595767b61b
mochafreddo <76798197+mochafreddo@users.noreply.github.com>
2023-03-18 16:34:36 +0900

    4.1 Form Submission

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
-  console.log(loginInput.value);
-  console.log("clicked!!!!");
+  const username = loginInput.value;
+  console.log(username);
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
