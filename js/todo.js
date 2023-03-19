1217785d014a2bb89e02215dda82c45f64fa9f01
mochafreddo <76798197+mochafreddo@users.noreply.github.com>
2023-03-19 22:20:30 +0900

    7.3 Saving To Dos

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,12 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const doDos = [];
+
+function saveToDos() {
+  localStorage.setItem("todos", JSON.stringify(doDos));
+}
+
 function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
@@ -23,7 +29,9 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newToDo = toDoInput.value;
   toDoInput.value = "";
+  toDoList.push(newToDo);
   paintToDo(newToDo);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
