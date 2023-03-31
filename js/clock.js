c822804403662297de66c7dad0278f2df254525c
mochafreddo <76798197+mochafreddo@users.noreply.github.com>
2023-03-31 11:06:45 +0900

    Refactored clock.js

--- js/clock.js
+++ js/clock.js
@@ -1,12 +1,22 @@
-const clock = document.querySelector("h2#clock");
+// Select the clock element from the DOM
+const clock = document.querySelector('h2#clock');
 
-function getClock() {
-  const date = new Date();
-  const hours = String(date.getHours()).padStart(2, "0");
-  const minutes = String(date.getMinutes()).padStart(2, "0");
-  const seconds = String(date.getSeconds()).padStart(2, "0");
+// Define a function to update the clock
+function updateClock() {
+  // Get the current date and time
+  const now = new Date();
+
+  // Extract the hours, minutes, and seconds from the date object
+  const hours = now.getHours().toString().padStart(2, '0');
+  const minutes = now.getMinutes().toString().padStart(2, '0');
+  const seconds = now.getSeconds().toString().padStart(2, '0');
+
+  // Update the clock element with the current time
   clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
-getClock();
-setInterval(getClock, 1000);
+// Call the updateClock function once to initialize the clock
+updateClock();
+
+// Set an interval to call the updateClock function every second
+setInterval(updateClock, 1000);
