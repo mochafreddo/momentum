4dd5bfc17f209744121161ee91a8f82ac1bd4cac
mochafreddo <76798197+mochafreddo@users.noreply.github.com>
2023-03-31 09:47:40 +0900

    refactored background.js
    - Renamed the `images` variable to `imageFilenames` to better reflect what it contains.
    - Added comments to explain what each section of code does.
    - Renamed `chosenImage` to `chosenImageFilename` to clarify what it represents.
    - Renamed `bgImage` to `backgroundImageElement` to better describe what it is.
    - Used single quotes for string literals, which is a common convention in JavaScript.
    - Used more descriptive variable names in general to improve readabitlity.

--- js/background.js
+++ js/background.js
@@ -1,9 +1,13 @@
-const images = ["0.jpg", "1.jpg", "2.jpg"];
+// Declare and initialize an array of image filenames
+const imageFilenames = ['0.jpg', '1.jpg', '2.jpg'];
 
-const chosenImage = images[Math.floor(Math.random() * images.length)];
+// Choose a random image filename from the array
+const chosenImageFilename =
+  imageFilenames[Math.floor(Math.random() * imageFilenames.length)];
 
-const bgImage = document.createElement("img");
+// Create a new image element and set its source attribute to the chosen image
+const backgroundImageElement = document.createElement('img');
+backgroundImageElement.src = `img/${chosenImageFilename}`;
 
-bgImage.src = `img/${chosenImage}`;
-
-document.body.appendChild(bgImage);
+// Add the image element to the body of the document
+document.body.appendChild(backgroundImageElement);
