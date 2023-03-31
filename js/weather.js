167889ce08b1d1706db2f94a86cd6361b623377c
mochafreddo <76798197+mochafreddo@users.noreply.github.com>
2023-03-31 09:26:31 +0900

    refactored weather.js
    - Added comments to explain each step and function.
    - Used destructuring to extract the `latitude` and `longitude` properties from `position.coords`.
    - Wrapped the `fetch` call in a `catch` block to handle any errors that occur during the API fetch.
    - Changed the error message in `onGeoError()` to make it more user-friendly.
    - Renamed the function parameters to be more descriptive.
    - Used template literals to construct the weather text for the HTML elements.

--- js/weather.js
+++ js/weather.js
@@ -1,21 +1,35 @@
-const API_KEY = "";
+// Define the API key as a constant
+const API_KEY = 'b1b35bba8b434a28a0be2a3e1071ae5b';
 
+// Handle successful geolocation lookup
 function onGeoOk(position) {
-  const lat = position.coords.latitude;
-  const lon = position.coords.longitude;
+  // Extract latitude and longitude from the goeloaction coordinates
+  const { latitude: lat, longitude: lon } = position.coords;
+
+  // Construct the API URL using the coordinates and API key
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+  // Fetch the weather data from the API using the contructed URL
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      const weather = document.querySelector("#weather span:first-child");
-      const city = document.querySelector("#weather span:last-child");
+      // Update the weather and city elements in the HTML with the fetched data
+      const weather = document.querySelector('#weather span:first-child');
+      const city = document.querySelector('#weather span:last-child');
       city.innerText = data.name;
       weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    })
+    .catch((error) => {
+      // Handle any errors that occur during the API fetch
+      console.error('Error fetching weather data: ', error);
+      alert('Error fetching weather data. Please try again later.');
     });
 }
 
+// Handle geolocation errors
 function onGeoError() {
-  alert("Can't find you. No weather for you.");
+  alert("Can't find your location. No weather data for you.");
 }
 
+// Call the geolocation API to get the user's current position
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
