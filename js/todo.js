a138bf59113ce0dbd2747c1f8efe22b11529b42f
mochafreddo <76798197+mochafreddo@users.noreply.github.com>
2023-03-31 09:40:36 +0900

    refactored todo.js
    - Added comments to explain each step and function.
    - Added input validation to `handleToDoSubmit()` to prevent empty to-do items from being added.
    - Used `trim()` to remove whitespace from the beginning and end of the new to-do item.
    - Changed the `if` statement to a guard clause to make the code more concise.

--- js/todo.js
+++ js/todo.js
@@ -1,15 +1,18 @@
-const toDoForm = document.getElementById("todo-form");
-const toDoInput = document.querySelector("#todo-form input");
-const toDoList = document.getElementById("todo-list");
+// Get the HTML elements and define the local storage key
+const toDoForm = document.getElementById('todo-form');
+const toDoInput = document.querySelector('#todo-form input');
+const toDoList = document.getElementById('todo-list');
+const TODOS_KEY = 'todos';
 
-const TODOS_KEY = "todos";
-
+// Initialize the to-do list array
 let toDos = [];
 
+// Save the to-do list to local storage
 function saveToDos() {
   localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
+// Remove a to-do item from the listand from local storage
 function deleteToDo(event) {
   const li = event.target.parentElement;
   li.remove();
@@ -17,23 +20,28 @@ function deleteToDo(event) {
   saveToDos();
 }
 
+// Add a new to-do item to the list
 function paintToDo(newToDo) {
-  const li = document.createElement("li");
+  const li = document.createElement('li');
   li.id = newToDo.id;
-  const span = document.createElement("span");
+  const span = document.createElement('span');
   span.innerText = newToDo.text;
-  const button = document.createElement("button");
-  button.innerText = "❌";
-  button.addEventListener("click", deleteToDo);
+  const button = document.createElement('button');
+  button.innerText = '❌';
+  button.addEventListener('click', deleteToDo);
   li.appendChild(span);
   li.appendChild(button);
   toDoList.appendChild(li);
 }
 
+// Handle submission of the to-do form
 function handleToDoSubmit(event) {
   event.preventDefault();
-  const newToDo = toDoInput.value;
-  toDoInput.value = "";
+  const newToDo = toDoInput.value.trim();
+  if (newToDo === '') {
+    return;
+  }
+  toDoInput.value = '';
   const newToDoObj = {
     text: newToDo,
     id: Date.now(),
@@ -43,10 +51,11 @@ function handleToDoSubmit(event) {
   saveToDos();
 }
 
-toDoForm.addEventListener("submit", handleToDoSubmit);
+// Add a submit event listener to the to-do form
+toDoForm.addEventListener('submit', handleToDoSubmit);
 
+// Load the to-do list from local storage if it exists
 const savedToDos = localStorage.getItem(TODOS_KEY);
-
 if (savedToDos !== null) {
   const parsedToDos = JSON.parse(savedToDos);
   toDos = parsedToDos;
