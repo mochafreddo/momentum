8dc64e9f5e7b1367d4a223f10fb1b3aaf34a3e99
mochafreddo <76798197+mochafreddo@users.noreply.github.com>
2023-04-21 22:09:52 +0900

    Update index.js

--- challenge/05/src/index.js
+++ challenge/05/src/index.js
@@ -1,19 +1,40 @@
-const body = document.body;
-const h1 = document.createElement("h1");
+const COLORS = {
+  HEADING: '#ecf0f1',
+  BACKGROUND_YELLOW: '#f1c40f',
+  BACKGROUND_PURPLE: '#9b59b6',
+  BACKGROUND_BLUE: '#2980b9',
+};
 
-body.style.backgroundColor = "#f1c40f";
-h1.textContent = "Hello!";
-h1.style.color = "#ecf0f1";
-body.appendChild(h1);
+function createHeading() {
+  const h1 = document.createElement('h1');
+  h1.textContent = 'Hello!';
+  h1.style.color = COLORS.HEADING;
+  return h1;
+}
 
-window.addEventListener("resize", () => {
-  const width = window.innerWidth;
-
-  if (width >= 0.5 * window.screen.width) {
-    body.style.backgroundColor = "#f1c40f";
-  } else if (width >= 0.3 * window.screen.width) {
-    body.style.backgroundColor = "#9b59b6";
+function setBackgroundColor(width) {
+  if (width >= 0.7 * window.screen.width) {
+    return COLORS.BACKGROUND_YELLOW;
+  } else if (width >= 0.4 * window.screen.width) {
+    return COLORS.BACKGROUND_PURPLE;
   } else {
-    body.style.backgroundColor = "#2980b9";
+    return COLORS.BACKGROUND_BLUE;
   }
-});
+}
+
+function updateBackgroundColor() {
+  const width = window.innerWidth;
+  document.body.style.backgroundColor = setBackgroundColor(width);
+}
+
+function initialize() {
+  const body = document.body;
+  const h1 = createHeading();
+
+  body.style.backgroundColor = COLORS.BACKGROUND_YELLOW;
+  body.appendChild(h1);
+
+  window.addEventListener('resize', updateBackgroundColor);
+}
+
+initialize();
