62aeda1fdf1fdedb2940d8c2c39a1c3e794f282a
mochafreddo <76798197+mochafreddo@users.noreply.github.com>
2023-04-25 15:14:32 +0900

    Nomad 🚀 Study Day 23 of 70

--- challenge/10/src/index.js
+++ challenge/10/src/index.js
@@ -1,54 +1,59 @@
-const clockTitle = document.querySelector(".js-clock");
+// Select the element that display the countdown clock
+const clockTitle = document.querySelector('.js-clock');
+const currentYear = new Date().getFullYear();
+// Set the Christmas date for the current year
+const CHRISTMAS_DAY = new Date(`December 25, ${currentYear}`);
 
-const CHRISTMAS_DAY = new Date(`December 25, ${new Date().getFullYear()}`);
+// Define time conversion constants
+const MILLISECONDS_PER_SECOND = 1000;
+const SECONDS_PER_MINUTE = 60;
+const MINUTES_PER_HOUR = 60;
+const HOURS_PER_DAY = 24;
 
+// Function to format time units with leading zeros (if needed)
+function formatTimeUnit(timeUnit) {
+  return timeUnit.toString().padStart(2, '0');
+}
+
+// Function to update the countdown clock
 function updateClock() {
+  // Get the current date and time
   const currentTime = new Date();
+  // Calculate the time difference between Christmas and the current time
   const timeDifference = CHRISTMAS_DAY.getTime() - currentTime.getTime();
 
-  const MILLISECONDS_PER_SECOND = 1000;
-  const SECONDS_PER_MINUTE = 60;
-  const MINUTES_PER_HOUR = 60;
-  const HOURS_PER_DAY = 24;
-
+  // Calculate the remaining days, hours, minutes, and seconds
   const daysLeft = Math.floor(
     timeDifference /
       (MILLISECONDS_PER_SECOND *
         SECONDS_PER_MINUTE *
         MINUTES_PER_HOUR *
-        HOURS_PER_DAY)
+        HOURS_PER_DAY),
   );
   const hoursLeft = Math.floor(
     (timeDifference /
       (MILLISECONDS_PER_SECOND * SECONDS_PER_MINUTE * MINUTES_PER_HOUR)) %
-      HOURS_PER_DAY
+      HOURS_PER_DAY,
   );
   const minutesLeft = Math.floor(
     (timeDifference / (MILLISECONDS_PER_SECOND * SECONDS_PER_MINUTE)) %
-      MINUTES_PER_HOUR
+      MINUTES_PER_HOUR,
   );
   const secondsLeft = Math.floor(
-    (timeDifference / MILLISECONDS_PER_SECOND) % SECONDS_PER_MINUTE
+    (timeDifference / MILLISECONDS_PER_SECOND) % SECONDS_PER_MINUTE,
   );
 
-  const TWO_DIGIT_NUMBER = 10;
-  const HUNDRED_DIGIT_NUMBER = 100;
+  // Format the remaining days, hours, minutes, and seconds with leading zeros (if needed)
+  const formattedDaysLeft = formatTimeUnit(daysLeft);
+  const formattedHoursLeft = formatTimeUnit(hoursLeft);
+  const formattedMinutesLeft = formatTimeUnit(minutesLeft);
+  const formattedSecondsLeft = formatTimeUnit(secondsLeft);
 
-  const formattedDaysLeft =
-    daysLeft < TWO_DIGIT_NUMBER
-      ? `00${daysLeft}`
-      : daysLeft < HUNDRED_DIGIT_NUMBER
-      ? `0${daysLeft}`
-      : daysLeft;
-  const formattedHoursLeft =
-    hoursLeft < TWO_DIGIT_NUMBER ? `0${hoursLeft}` : hoursLeft;
-  const formattedMinutesLeft =
-    minutesLeft < TWO_DIGIT_NUMBER ? `0${minutesLeft}` : minutesLeft;
-  const formattedSecondsLeft =
-    secondsLeft < TWO_DIGIT_NUMBER ? `0${secondsLeft}` : secondsLeft;
-
+  // Update the counddown clock element's contect with the formatted remaining time
   clockTitle.innerHTML = `${formattedDaysLeft}d ${formattedHoursLeft}h ${formattedMinutesLeft}m ${formattedSecondsLeft}s`;
 }
 
+// Call the updateClock function to display the initial countdown time
 updateClock();
+// Set an interval to update the countdown clock every second
 setInterval(updateClock, 1000);
